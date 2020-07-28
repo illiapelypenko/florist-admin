38f4cfcb8d7a2e8b734f5294fdcc36bbc99528e4
IlliaPelypenko <pelypenkoillya@gmail.com>
2020-07-28 22:19:24 +0300

    change doc title

--- src/reducers/reducers.js
+++
@@ -1,7 +0,0 @@
-import { FETCH_ITEMS, FETCH_TYPES, FETCH_CONTACTS } from '../actions/actions';
-
-export function itemsReducer(state = [], action) {
-  switch (action.type) {
-    case FETCH_ITEMS:
-  }
-}
